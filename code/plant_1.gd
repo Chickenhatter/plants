618f4e0f6b9c66653c7f10f1662bb18563be319a
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-06-22 20:35:38 +1200

    3hr 2m, plant adding scale goes down

--- code/plant_1.gd
+++ code/plant_1.gd
@@ -14,17 +14,24 @@ func _process(_delta: float) -> void:
 		if Input.is_action_just_pressed('ui_m'):
 			if global.player_is_holding == "Water":
 				print('a')
-				water_level += 10
+				water_level += 40
 	if gardener_in == true:
 		if Input.is_action_just_pressed('ui_m'):
 			if global.player_is_holding == "Nitrogen":
-				nitrogen_level += 10
+				nitrogen_level += 40
 	if gardener_in == true:
 		if Input.is_action_just_pressed('ui_m'):
 			global.player_is_holding = "None"
-	nitrogen_level -= 0.01
-	water_level -= 0.01
-
+	nitrogen_level -= 0.04
+	water_level -= 0.04
+	if nitrogen_level > 0:
+		$Node2D/nitrogen.scale.y = nitrogen_level/100
+	else:
+		$Node2D/health.scale.y -= 0.01/100
+	if water_level > 0:
+		$Node2D/water.scale.y = nitrogen_level/100
+	else:
+		$Node2D/health.scale.y -= 0.01/100
 
 func _on_plant_1_body_entered(body: Node2D) -> void:
 	if body.name == "Gardener":
--- code/plant_2.gd
+++ code/plant_2.gd
@@ -1,33 +1,45 @@
 extends Node2D
-var player_in = false
-var water_level = 70
+var gardener_in = false
 var nitrogen_level = 70
+var water_level = 70
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if player_in == true:
+func _process(_delta: float) -> void:
+	print(nitrogen_level)
+	print(water_level)
+	if gardener_in == true:
 		if Input.is_action_just_pressed('ui_m'):
 			if global.player_is_holding == "Water":
+				print('a')
 				water_level += 40
-	if player_in == true:
+	if gardener_in == true:
 		if Input.is_action_just_pressed('ui_m'):
 			if global.player_is_holding == "Nitrogen":
 				nitrogen_level += 40
-	
-	water_level -= 0.01
-	nitrogen_level -= 0.01
+	if gardener_in == true:
+		if Input.is_action_just_pressed('ui_m'):
+			global.player_is_holding = "None"
+	nitrogen_level -= 0.04
+	water_level -= 0.04
+	if nitrogen_level > 0:
+		$Node2D/nitrogen.scale.y = nitrogen_level/100
+	else:
+		$Node2D/health.scale.y -= 0.01/100
+	if water_level > 0:
+		$Node2D/water.scale.y = nitrogen_level/100
+	else:
+		$Node2D/health.scale.y -= 0.01/100
 
-func _on_plant_2_body_entered(body: Node2D) -> void:
+func _on_plant_1_body_entered(body: Node2D) -> void:
 	if body.name == "Gardener":
-		player_in = true
+		gardener_in = true
 		global.player_in_plant = true
 
 
-func _on_plant_2_body_exited(body: Node2D) -> void:
+func _on_plant_1_body_exited(body: Node2D) -> void:
 	if body.name == "Gardener":
-		player_in = false
+		gardener_in = false
 		global.player_in_plant = false
