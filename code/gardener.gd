b4f4f3876e306ecb185d2bf168dde8f24e3c95e9
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-06-26 16:09:21 +1200

    8hr 5m added new image and editing character for the image

--- code/gardener.gd
+++ code/gardener.gd
@@ -1,20 +1,26 @@
 extends CharacterBody2D
-
+var move = 0
+var timser = 1
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
+	print(sin(move))
 	var movement = Vector2.ZERO
 	if global.player_move == true:
 		if Input.is_action_pressed('ui_w'):
+			move += delta * 10 * timser
 			movement.y -= 1
 		if Input.is_action_pressed('ui_s'):
+			move += delta * 10 * timser
 			movement.y += 1
 		if Input.is_action_pressed('ui_a'):
+			move += delta * 10 * timser
 			movement.x -= 1
 		if Input.is_action_pressed('ui_d'):
+			move += delta * 10 * timser
 			movement.x += 1
 	velocity = movement * 150
 	global.character_position = $".".global_position
